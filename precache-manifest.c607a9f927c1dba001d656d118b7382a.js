8b8a452ba717ccd95e9a7535ac7a2f1b0aadf653
huongse7en <huongse7en>
2020-01-17 14:17:20 +0700

    zip

---
+++ precache-manifest.c607a9f927c1dba001d656d118b7382a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4473a7cdc88beeae32ad6e0d3f7643fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "826605b299bc46c83346",
+    "url": "/static/css/2.3a45b1c2.chunk.css"
+  },
+  {
+    "revision": "841530463bc841d15e92",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "826605b299bc46c83346",
+    "url": "/static/js/2.687b2735.chunk.js"
+  },
+  {
+    "revision": "2821a807f373b962ca3ae8bc13db778f",
+    "url": "/static/js/2.687b2735.chunk.js.LICENSE"
+  },
+  {
+    "revision": "841530463bc841d15e92",
+    "url": "/static/js/main.8f64c640.chunk.js"
+  },
+  {
+    "revision": "861e4233396253aee4bd",
+    "url": "/static/js/runtime-main.07fa7236.js"
+  }
+]);
